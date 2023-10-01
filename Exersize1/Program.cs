9e2a6680bb478b0213b91d291d8178d3aae8f32f
Andrei Strakhov <straxarts@outlook.com>
2023-10-01 21:06:46 +0300

    создал репозиторий, выполнил Задание 1 из домашнего задания 1

---
+++ Exersize1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max < b)
+{
+Console.WriteLine ("Второе число максимальное, а первое число минимальное");
+}
+else if (max == b)
+{
+Console.WriteLine ("Числа равны");
+}
+else
+{
+Console.WriteLine ("Первое число максимальное, а второе число минимальное"); 
+}
+
+
+
+    
